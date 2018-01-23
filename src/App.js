2e9cb39738332f9a22ec470ded6f055892ea48fd
yukixoma <yukixoma08@gmail.com>
2018-01-23 18:57:26 +0700

    toppanel

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
+import { HashRouter, Route } from 'react-router-dom';
 
 
 class App extends Component {
     render() {
         return (
-            <Fragment>
-                <Header />
-                <Navbar />
-            </Fragment>
+            <HashRouter>
+                <Fragment>
+                    <Header />
+                    <Navbar />
+                </Fragment>
+            </HashRouter>
         )
     }
 }
---
+++ src/components/Main/TopPanel.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class TopPanel extends Component {
+    render () {
+        return (
+            <div className="card">
+                <h3 className="card-header">Top Panel</h3>
+                <div className="card-block">
+                    <div className="row">
+                        <div>
+                            <div className="card">
+                                <img className="card-img-top" src="" alt="Card image cap"/>
+                                <div className="card-block">
+                                    <h3 className="card-title">Special title treatment</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div className="card">
+                                <img className="card-img-top" src="" alt="Card image cap"/>
+                                <div className="card-block">
+                                    <h3 className="card-title">Special title treatment</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default TopPanel;
